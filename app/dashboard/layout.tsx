c08598c636044fa77b336659d0098b33a45c6806
ibanlopez <lopez.iban@gmail.com>
2023-10-28 16:20:03 +0200

    fix: bugfixes to make it work for deployment

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,13 +2,13 @@ import SideNav from '@/app/ui/dashboard/sidenav'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="flex h-screen flex-col md:flew-row md:overflow-hidden">
+		<div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
 			<header className="w-full flex-none md:w-64">
 				<SideNav />
 			</header>
-			<main className="flex-grow p-6 md:overflow-y-auto md:p-12">
+			<div className="flex-grow p-6 md:overflow-y-auto md:p-12">
 				{children}
-			</main>
+			</div>
 		</div>
 	)
 }
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
 type Revenue = {
 	month: string
 	revenue: number
-}
+}[]
